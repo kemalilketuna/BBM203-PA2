352b9493bdeeb44358cd859766c016cd76369157
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-11-21 15:38:59 +0300

    leaderboard done

--- BlockFall.cpp
+++ BlockFall.cpp
@@ -19,17 +19,51 @@ void BlockFall::read_blocks(const string &input_file) {
     // TODO: Initialize the "power_up" member variable as the last block from the input file (do not add it to the linked list!)
     
     // open the file
+    cout << "reading blocks" << endl;
     ifstream file(input_file);
     string line;
 
     vector<vector<bool>> matrix;
     while (getline(file, line))
     {
-    
+        if (line[0] != '\r'){
+            vector<bool> row;
+            for (char c : line){
+                if (c == '1'){
+                    row.push_back(true);
+                }
+                else if (c == '0'){
+                    row.push_back(false);
+                }
+            }
+            matrix.push_back(row);
+        }else{
+            Block * block = new Block();
+            block->set_shape(matrix);
+            if (initial_block == nullptr){
+                initial_block = block;
+                active_rotation = block;
+            }else{
+                active_rotation->set_next_block(block);
+            }
+            //clear the matrix
+            matrix.clear();
+        }
     }
+
+    power_up = active_rotation->next_block->shape;
+    delete active_rotation->next_block;
     
     // close the file
     file.close();
+
+    Block * temp = initial_block;
+    while (temp != nullptr){
+        cout << temp->shape.size() << 'x' << temp->shape.at(0).size() << endl;
+        temp = temp->next_block;
+    }
+    cout << "initial block is null" << endl;
+    
 }
 
 void BlockFall::initialize_grid(const string &input_file) {
@@ -64,5 +98,7 @@ void BlockFall::initialize_grid(const string &input_file) {
 BlockFall::~BlockFall() {
     // TODO: Free dynamically allocated memory used for storing game blocks
     //implement recursive delete function in block.h
-    // initial_block->delete_block();
+    if (initial_block != nullptr){
+        initial_block->delete_block();
+    }
 }
--- Leaderboard.cpp
+++ Leaderboard.cpp
@@ -1,26 +1,97 @@
 #include "Leaderboard.h"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
 
-void Leaderboard::insert_new_entry(LeaderboardEntry * new_entry) {
-    // TODO: Insert a new LeaderboardEntry instance into the leaderboard, such that the order of the high-scores
-    //       is maintained, and the leaderboard size does not exceed 10 entries at any given time (only the
-    //       top 10 all-time high-scores should be kept in descending order by the score).
-}
-
-void Leaderboard::write_to_file(const string& filename) {
-    // TODO: Write the latest leaderboard status to the given file in the format specified in the PA instructions
-}
-
-void Leaderboard::read_from_file(const string& filename) {
-    // TODO: Read the stored leaderboard status from the given file such that the "head_leaderboard_entry" member
-    //       variable will point to the highest all-times score, and all other scores will be reachable from it
-    //       via the "next_leaderboard_entry" member variable pointer.
-}
-
-
-void Leaderboard::print_leaderboard() {
-    // TODO: Print the current leaderboard status to the standard output in the format specified in the PA instructions
-}
-
+// Destructor
 Leaderboard::~Leaderboard() {
-    // TODO: Free dynamically allocated memory used for storing leaderboard entries
+    while (head_leaderboard_entry != nullptr) {
+        LeaderboardEntry* temp = head_leaderboard_entry;
+        head_leaderboard_entry = head_leaderboard_entry->next_leaderboard_entry;
+        delete temp;
+    }
+}
+
+// Insert a new entry
+void Leaderboard::insert_new_entry(LeaderboardEntry* new_entry) {
+    if (!head_leaderboard_entry || new_entry->score > head_leaderboard_entry->score) {
+        new_entry->next_leaderboard_entry = head_leaderboard_entry;
+        head_leaderboard_entry = new_entry;
+    } else {
+        LeaderboardEntry* current = head_leaderboard_entry;
+        while (current->next_leaderboard_entry && current->next_leaderboard_entry->score >= new_entry->score) {
+            current = current->next_leaderboard_entry;
+        }
+        new_entry->next_leaderboard_entry = current->next_leaderboard_entry;
+        current->next_leaderboard_entry = new_entry;
+    }
+
+    // Trimming the leaderboard
+    LeaderboardEntry* temp = head_leaderboard_entry;
+    int count = 1;
+    while (temp && temp->next_leaderboard_entry && count < MAX_LEADERBOARD_SIZE) {
+        temp = temp->next_leaderboard_entry;
+        count++;
+    }
+
+    if (temp->next_leaderboard_entry) {
+        LeaderboardEntry* excess = temp->next_leaderboard_entry;
+        temp->next_leaderboard_entry = nullptr;
+        while (excess) {
+            LeaderboardEntry* next = excess->next_leaderboard_entry;
+            delete excess;
+            excess = next;
+        }
+    }
+}
+
+// Write to file
+void Leaderboard::write_to_file(const string& filename) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file for writing: " << filename << std::endl;
+        return;
+    }
+
+    LeaderboardEntry* current = head_leaderboard_entry;
+    while (current != nullptr) {
+        file << current->score << " " << current->last_played << " " << current->player_name << "\n";
+        current = current->next_leaderboard_entry;
+    }
+
+    file.close();
+}
+
+// Read from file
+void Leaderboard::read_from_file(const string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file for reading: " << filename << std::endl;
+        return;
+    }
+
+    unsigned long score;
+    time_t lastPlayed;
+    string playerName;
+
+    while (file >> score >> lastPlayed >> playerName) {
+        LeaderboardEntry* newEntry = new LeaderboardEntry(score, lastPlayed, playerName);
+        insert_new_entry(newEntry);
+    }
+
+    file.close();
+}
+
+// Print the leaderboard
+void Leaderboard::print_leaderboard() {
+    LeaderboardEntry* current = head_leaderboard_entry;
+    int rank = 1;
+
+    std::cout << "Leaderboard\n-----------\n";
+    while (current != nullptr) {
+        std::cout << rank << ". " << current->player_name << " " << current->score << " ";
+        std::cout << std::put_time(std::localtime(&current->last_played), "%H:%M:%S/%d.%m.%Y") << std::endl;
+        current = current->next_leaderboard_entry;
+        rank++;
+    }
 }
