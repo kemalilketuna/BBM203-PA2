7387d248c38c5c02d20c049a12caf8f59a4fa09c
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-11-24 20:54:58 +0300

    game controller

--- GameController.cpp
+++ GameController.cpp
@@ -1,13 +1,70 @@
 #include "GameController.h"
+#include <iostream>
+#include <fstream>
 
 bool GameController::play(BlockFall& game, const string& commands_file){
 
-    // TODO: Implement the gameplay here while reading the commands from the input file given as the 3rd command-line
-    //       argument. The return value represents if the gameplay was successful or not: false if game over,
-    //       true otherwise.
+    ifstream file(commands_file);
+    std::string line;
+
+    while (getline(file, line))
+    {
+        //PRINT_GRID
+        if(line == "PRINT_GRID"){
+            //do something
+        }
+        //ROTATE_RIGHT
+        else if(line == "ROTATE_RIGHT"){
+            //do something
+        }
+        //ROTATE_LEFT
+        else if(line == "ROTATE_LEFT"){
+            //do something
+        }
+        //MOVE_RIGHT
+        else if(line == "MOVE_RIGHT"){
+            //do something
+        }
+        //MOVE_LEFT
+        else if(line == "MOVE_LEFT"){
+            //do something
+        }
+        //GRAVITY_SWITCH
+        else if(line == "GRAVITY_SWITCH"){
+            //do something
+        }
+        //DROP
+        else if(line == "DROP"){
+            //do something
+        }
+    }
 
     return false;
 }
 
+int GameController::athHighScore(BlockFall &game){
+    if(game.leaderboard.head_leaderboard_entry == nullptr){
+        return this->current_score;
+    }else if(game.leaderboard.head_leaderboard_entry->score < this->current_score){
+        return this->current_score;
+    }else{
+        return game.leaderboard.head_leaderboard_entry->score;
+    }
+}
+
+void GameController::print_grid(BlockFall &game){
+    cout << "Score: " << this->current_score << endl;
+    cout << "High Score: " << athHighScore(game) << endl;
+    for(int i = 0; i < game.rows; i++){
+        for(int j = 0; j < game.cols; j++){
+            if(game.grid[i][j] == 1){
+                cout << occupiedCellChar;
+            }else{
+                cout << unoccupiedCellChar;
+            }
+        }
+        cout << endl;
+    }
+}
 
 
--- GameController.h
+++ GameController.h
@@ -8,6 +8,10 @@ using namespace std;
 class GameController {
 public:
     bool play(BlockFall &game, const string &commands_file); // Function that implements the gameplay
+private:
+    int current_score = 0;
+    int athHighScore(BlockFall &game);
+    void print_grid(BlockFall &game);
 };
 
 
